1f6a48533ec9b6d9714b5dbd9fc776d25913d1b6
Evan Guo <evanguo97@gmail.com>
2020-10-09 23:35:05 -0700

    fixed wolfgoatcabbage problem

--- csc421/a1/search_post/src/ProblemPancake.java
+++ csc421/a1/search_post/src/ProblemPancake.java
@@ -54,7 +54,6 @@ public class ProblemPancake extends Problem {
         return sum;
     }
 
-
     public static void main(String[] args) throws Exception {
         ProblemPancake problem = new ProblemPancake();
         int[] pancakeArray = {1, 0, 3, 5, 2, 4};
@@ -64,13 +63,13 @@ public class ProblemPancake extends Problem {
 
         System.out.println("TreeSearch------------------------");
         //System.out.println("BreadthFirstTreeSearch:\t\t" + search.BreadthFirstTreeSearch());
-        System.out.println("UniformCostTreeSearch:\t\t" + search.UniformCostTreeSearch());
+        //System.out.println("UniformCostTreeSearch:\t\t" + search.UniformCostTreeSearch());
         //System.out.println("DepthFirstTreeSearch:\t\t" + search.DepthFirstTreeSearch());
         //System.out.println("GreedyBestFirstTreeSearch:\t" + search.GreedyBestFirstTreeSearch());
-        System.out.println("AstarTreeSearch:\t\t" + search.AstarTreeSearch());
+        //System.out.println("AstarTreeSearch:\t\t" + search.AstarTreeSearch());
 
         System.out.println("\n\nGraphSearch----------------------");
-        //System.out.println("BreadthFirstGraphSearch:\t" + search.BreadthFirstGraphSearch());
+        System.out.println("BreadthFirstGraphSearch:\t" + search.BreadthFirstGraphSearch());
         System.out.println("UniformCostGraphSearch:\t\t" + search.UniformCostGraphSearch());
         //System.out.println("DepthFirstGraphSearch:\t\t" + search.DepthFirstGraphSearch());
         //System.out.println("GreedyBestGraphSearch:\t\t" + search.GreedyBestFirstGraphSearch());
@@ -78,7 +77,7 @@ public class ProblemPancake extends Problem {
 
         System.out.println("\n\nIterativeDeepening----------------------");
         //System.out.println("IterativeDeepeningTreeSearch:\t" + search.IterativeDeepeningTreeSearch());
-        //System.out.println("IterativeDeepeningGraphSearch:\t" + search.IterativeDeepeningGraphSearch());
+        System.out.println("IterativeDeepeningGraphSearch:\t" + search.IterativeDeepeningGraphSearch());
     }
 
 }
